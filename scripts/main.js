35bce28113e98c5275cd907a84c6d9fb270255f4
VicooDi <vicoodigm@gmail.com>
2024-07-08 20:59:57 +0200

    reworked the entier thing

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+
+function tabsUp() {
+    var tabs = document.getElementById("tabs");
+    var sections = document.getElementsByClassName("sections");
+    if(tabs.matches(":hover")){
+        return 0
+    }
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].style.animation="tabs_up 0.2s ease";
+        sections[i].style.animationFillMode="forwards";
+    }
+    //tabs.style.animation="tabs_up 0.2s ease";
+    //tabs.style.animationFillMode="forwards";
+    return 1;
+}
+function tabsDown() {
+    var sections = document.getElementsByClassName("sections");
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].style.animation="tabs_down 0.2s ease";
+        sections[i].style.animationFillMode="forwards";
+    }
+
+    return 1;
+}
