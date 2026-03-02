d2a56eb78304d844d2f62576141ce576cc4690b1
VicooDi <jihedbenslimen54@gmail.com>
2026-03-02 02:15:56 +0200

    re-implemented lost features. fixed base files and model loading issues

--- src/lib/3d_manager/logo.js
+++ src/lib/3d_manager/logo.js
@@ -3,10 +3,10 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
-import { load_model } from '.';
+import { load_model, eventBus } from '.';
 import { model_loader, texture_loader, scene, camera, renderer } from '.';
 
-alert("I AM BIRTHED!!");
+// alert("I AM BIRTHED!!");
 
 // ============ link to hmtl container =======
 
@@ -14,27 +14,72 @@ const container = document.getElementById("me");
 
 // ============ Set Paramaters ===============
 
-renderer.setSize( container.clientWidth, container.clientHeight );
+camera.aspect = container.clientWidth / container.clientHeight
+
+renderer.setSize( container.clientWidth+64, container.clientHeight ); //these params are really messy FIX IT FUTURE ME!!
 container.appendChild(renderer.domElement);
+
+// ============ Fix CSS Spacing ==============
+
+renderer.domElement.style.margin = 0;
 
 //most cameras would be facing X, so the X value represents scale rather then distance
 //this is further cemented with the camera being perspective yet having a very low fov
-camera.position.set(35, 0, 0);
+camera.position.set(55, 0, 0);
+camera.lookAt(scene.position); //points at the scene origin, change to model origin upon any issue arizing.
 
 // ============ Create Model =================
 
+// renderer.domElement.focus({ preventScroll : true ,focusVisible : true});
 load_model('/3d_models/logo.glb');
 
 function animate( time ) {
   renderer.render(scene, camera);
 }
-renderer.setAnimationLoop( animate );
+renderer.setAnimationLoop(animate);
 
 // ============ Specific Functions ===========
 
-onmousemove = function(e){
-  let posX = e.clientX;
-  let posY = e.clientY;
+// eventEmitter.emit('_onloaded', () => {
+//   console.log('started');
+// });
+var logo;
+var eyes;
 
-//   console.log("mouse location:", posX, posY);
+var loaded = false;
+eventBus.addEventListener('_onloaded', (e) => {
+  console.log('model loaded:', e.detail);
+  logo = e.detail.sceneObject;
+  eyes = e.detail.sceneObject.getObjectByName("Circle");
+  // e.detail.sceneObject is the loaded THREE.Group/Scene
+  // set flags, assign to local variables, etc.
+  // e.g. logo = e.detail.sceneObject;
+});
+
+function getObjectPagePosition(object, camera, rendererDom) {
+  let pos = new THREE.Vector3();
+  object.getWorldPosition(pos);
+  pos.project(camera);
+
+  const rect = rendererDom.getBoundingClientRect(); // CSS pixels
+  const x = pos.x + (rect.width/2) + rect.left; //pass to vector directly to avoid pointless declarations.
+  const y = -pos.y + (rect.height/2) + rect.top;
+  return new THREE.Vector3(x, y, 0);
+}
+
+
+var vec = new THREE.Vector3();
+onmousemove = function (e) {
+  if (!(logo && eyes))
+    return;
+  var m_pos = new THREE.Vector3(e.clientX, e.clientY);
+
+  const pos = getObjectPagePosition(eyes, camera, renderer.domElement);
+  
+  vec = new THREE.Vector3(m_pos.x - pos.x, m_pos.y - pos.y);
+  vec.normalize();
+
+  eyes.position.set(0, -vec.y, -vec.x);
+
+  console.log("mouse location:", m_pos.x, m_pos.y);
 }
--- src/lib/3d_manager/object.js
+++ src/lib/3d_manager/object.js
@@ -4,11 +4,16 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 //import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { generate_random_color } from '/src/lib/utilities';
 
+//random bullshit i need
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+
 export const model_loader = new GLTFLoader();
 export const texture_loader = new THREE.TextureLoader();
 
 export const scene = new THREE.Scene();
-export const camera = new THREE.PerspectiveCamera(17, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+export const camera = new THREE.PerspectiveCamera(15, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 export const renderer = new THREE.WebGLRenderer({ alpha: true }); //TODO : test for failure
 
@@ -19,51 +24,55 @@ const controls = new OrbitControls(camera, renderer.domElement); //TODO : remove
 // renderer.setSize( window.innerWidth, window.innerHeight );
 // document.body.appendChild(renderer.domElement);
 
+//========= Emitters ============
+export const eventBus = new EventTarget();
+
 //exported functions
 /** function to load a 3D model, meant to be given minimal parameters
  * path : relativce path to model
  * (optional) material : model's material {THREE.MeshPhongMaterial} or {THREE.MeshBasicMaterial}
- * not providong a material result in random coloring.
-*/
+ * not providong a material result in random coloring.*/
 export function load_model(path, material = null) {
+    let res;
         model_loader.load(path, function (object) {
             //for debugging TODO : remove on release!!!
             if (material == null) {
                 object.scene.traverse(function(child) {
                     if (child.isMesh) {
-                        material = new THREE.MeshPhongMaterial({color: generate_random_color()});
+                        // material = new THREE.MeshPhongMaterial({color: generate_random_color()});
+                        material = new THREE.MeshBasicMaterial({color: generate_random_color()});
                         child.material = material;
+                        // child.frustumCulled = false;
                     }
                 });
             }
             else {
+                material = new THREE.MeshBasicMaterial({color: generate_random_color()});
                 object.scene.material = material;
             }
 
             //final
+            res = object.scene;
             scene.add(object.scene);
+            eventBus.dispatchEvent(new CustomEvent('_onloaded', { detail: { sceneObject: object.scene} }));
+
         }, function( xhr ){
                 console.log( (xhr.loaded / xhr.total * 100) + "% loaded")
         }, undefined, function ( error ) {
             console.error("error loading model : " + error);
             alert("failed to load model\nplease check the console");
         });
+    return res;
 }
 
 function loop( time ) { // somhow make it so all who inherit this code shall go throught the loop
     renderer.render( scene, camera );
 }
 
-
-function next_function () {
-    
-}
-
-
-//========= HOW TO USE : 
+//=========> HOW TO USE : 
 /*this script is supposed to be attached to any child script and used straight up, but s>certain js limitations 
 made this process more difficult, I know this language favors composition rather then inheritance,
-BUT STILL!! this is a lot more convuloted then any other language *even pythn!*
+BUT STILL!! this is a lot more convuloted then any other language *even python!*
 add this line : renderer.setAnimationLoop(loop);
 
 */
