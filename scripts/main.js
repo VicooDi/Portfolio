326743303a98ab9f717f5b1fe20f389302c9620f
VicooDi <jihedbenslimen54@gmail.com>
2026-05-31 23:54:56 +0200

    static revision

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,16 @@ var win_h = window.innerHeight;
 function startup(){
     document.body.innerWidth = win_w;
     document.body.innerHeight = win_h;
-
+    var tabs_moved = 0;
+    annoying = false;
+    var annoying2 = false;
     return 0;
 }
 
+var tabsUpTrue = false;
 function tabsUp() {
+    if (tabsUpTrue)
+        return 1;
     var tabs = document.getElementById("tabs");
     var sections = document.getElementsByClassName("sections");
     if(tabs.matches(":hover")){
@@ -21,15 +26,37 @@ function tabsUp() {
     }
     //tabs.style.animation="tabs_up 0.2s ease";
     //tabs.style.animationFillMode="forwards";
+    tabsUpTrue = true;
+    tabsDownTrue = false;
     return 1;
 }
+
+var annoying = false;
+var annoying2 = false;
+var tabsDownTrue = false;
 function tabsDown() {
+    if (tabsDownTrue)
+        return 1;
+
     var sections = document.getElementsByClassName("sections");
     for (let i = 0; i < sections.length; i++) {
         sections[i].style.animation="tabs_down 0.2s ease";
         sections[i].style.animationFillMode="forwards";
     }
-    tabs_moved+=1;
+
+    tabs_moved += 1;
+    
+    if (tabs_moved >10  && !annoying) {
+        alert("This is not a bug, I like being annoying.")
+        annoying = true
+    }
+    if (tabs_moved >20  && !annoying2) {
+        alert("matter of fact, I went out of my way to make this work")
+        annoying2 = true
+    }
+
+    tabsUpTrue = false;
+    tabsDownTrue = true;
     return 1;
 }
 
